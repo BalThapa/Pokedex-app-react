20e8303a4c4927868f5d1b4a98227af937e99202
BalThapa <info.balthapa@gmail.com>
2023-09-14 13:59:54 +0300

    added scripts

---
+++ scripts/start_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo chmod -R 777 /home/ec2-user/pokedex
+
+cd /home/ec2-user/pokedex
+
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+
+npm install
+
+npm start
